3f4f2abca0bf405381ee537544348fc577865e5c
kwamebio <kwameagyemang73@gmail.com>
2024-03-24 11:31:20 +0000

    first commit

---
+++ src/main/java/methods/GreetUser.java
@@ -0,0 +1,11 @@
+package methods;
+
+public class getUserName {
+    public static void main(String[] args) {
+        string name = getUserName();
+    }
+
+    public static String getUserName(){
+
+    }
+}
