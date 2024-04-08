f20fae8c6c65db368bbf27e9a0e6dd5ca2b8da05
kwamebio <kwameagyemang73@gmail.com>
2024-04-08 16:55:03 +0100

    default tag on the getters and setters of interfaces

--- src/main/java/interfaces/Product.java
+++ src/main/java/interfaces/Product.java
@@ -2,6 +2,14 @@ package interfaces;
 
 public interface Product {
     String getName();
-    void setName(String sadsa);
+    void setName(String string );
+
+    default double getPrice(){
+        return 50;
+    }
+    default void setPrice(double price){
+
+    }
+
 
 }
